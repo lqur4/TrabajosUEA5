4345cbe9846e148be620468d506c32d0097ece97
lqur4 <2233073359@correo.ler.uam.mx>
2025-02-25 10:16:16 -0600

    todo

---
+++ variables/script.js
@@ -0,0 +1,10 @@
+//variables
+
+let ingrediente1 = "azúcar"
+let precio1 =  46;    
+
+//document.write (ingrediente1);
+document.getElementById('demo').innerHTML= "Un ingrediente necesario:" + ingrediente1;
+
+//document.write (precio1);
+document.getElementById('demo1').innerHTML= "El precio del azúcar es: " + precio1;

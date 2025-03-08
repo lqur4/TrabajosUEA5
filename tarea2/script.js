010f774f30740f71ce715e41392cfaf9b3def4ea
lqur4 <2233073359@correo.ler.uam.mx>
2025-03-07 19:56:02 -0600

    tarea 2

--- tarea2/script.js
+++ tarea2/script.js
@@ -1,6 +1,9 @@
-let p1 = "hola bb jshdfjksdjk";
-let p2 = "párrafo 2";
+let p1 = "Las implicaciones que la tecnología web puede tener en mi formación son muchas, hoy en clase el profesor nos dio varios ejemplos del provecho que le podemos sacar a los conocimientos sobre programación que estamos adquiriendo en la universidad, algunas salidas laborales no tan lejanas y otras incluso en el extranjero. Me sentía un poco insegura sobre esta carrera, pero viéndolo desde ese punto de vista me doy cuenta de que aprender sobre HTML, CSS y JavaScript puede ser útil en la creación de experiencias, diseño de páginas web, contenido digital y marketing, entre otras cosas.";
+
+let p2 = "Cada día descubro herramientas diferentes digitales, hasta el trimestre pasado jamás usé algo siquiera parecido a Blender. También he visto que algunas personas bastante experimentadas en HTML han logrado hacer cosas increíbles y que si tengo la oportunidad quiero implementar en proyectos futuros. La mayoría de las herramientas que se proponen en clases son bastante útiles en los proyectos finales y es gratificante ir entendiendolas poco a poco, aún me falta mucho pero me siento orgullosa de inluso estar entregando esta tarea sin ayuda de nadie.";
 
 document.getElementById ('parr1').innerHTML= p1;
 document.getElementById ('parr2').innerHTML= p2;
 
+console.log(p1.length);
+console.log(p2.length);

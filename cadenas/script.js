05a9188612a3e26395824a6a5d7124f415fd0e2b
lqur4 <2233073359@correo.ler.uam.mx>
2025-03-11 10:59:30 -0600

    trabajo en clase

--- cadenas/script.js
+++ cadenas/script.js
@@ -1,9 +1,12 @@
-let fl = "skibidi sigma ponmi digital fornite chamba free giga chat fress omg flo flo flo xxxtentation Hotmail Lionel Ronaldo Junior Mewing Tercero Chiki Ibai Xocas Ete Sech Golden Toy Puppet Ohio Rubén Tuesta YouTubeproinsano Globodetexto51 Decadencia777 Digital, Digital Fortnite, Digital, Digital Flow Digital, Digital Fortnite, Digital, Digital Flow Digital, Digital Fortnite, Digital, Digital Flow Digital, Digital Fortnite, Digital, Digital Flow Skibidi, Skibidi, Skibidi, Skibidi Skibidi, Skibidi, Skibidi, Skibidi Digital, Digital Fortnite, Digital, Digital Flow Digital, Digital Fortnite, Digital, Digital Flow"
+let texto = "skibidi sigma ponmi digital fornite chamba free giga chat fress omg flo flo flo xxxtentation Hotmail Lionel Ronaldo Junior Mewing Tercero Chiki Ibai Xocas Ete Sech Golden Toy Puppet Ohio Rubén Tuesta YouTubeproinsano Globodetexto51 Decadencia777 Digital, Digital Fortnite, Digital, Digital Flow Digital, Digital Fortnite, Digital, Digital Flow Digital, Digital Fortnite, Digital, Digital Flow Digital, Digital Fortnite, Digital, Digital Flow Skibidi, Skibidi, Skibidi, Skibidi Skibidi, Skibidi, Skibidi, Skibidi Digital, Digital Fortnite, Digital, Digital Flow Digital, Digital Fortnite, Digital, Digital Flow"
+let texto2 = texto.toUpperCase();
+let texto3= texto.replace("frase", "fresita");
 
-document.getElementById(':p').innerHTML=fl;
+document.getElementById('cadena').innerHTML=texto;
+document.getElementById('cadena2').innerHTML=texto2;
+document.getElementById('cadena3').innerHTML=texto3;
 
 //consola
-
 //console.log (fl.length);
-
-console.log (fl.at(10));
+//console.log (fl.at(10));   
+console.log(texto.toUpperCase());

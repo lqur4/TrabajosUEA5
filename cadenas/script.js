b29dc4b24e2150412baa8cdd01c808cffd2a1bc0
lqur4 <2233073359@correo.ler.uam.mx>
2025-03-11 09:20:12 -0600

    cadenas

---
+++ cadenas/script.js
@@ -0,0 +1,9 @@
+let fl = "skibidi sigma ponmi digital fornite chamba free giga chat fress omg flo flo flo xxxtentation Hotmail Lionel Ronaldo Junior Mewing Tercero Chiki Ibai Xocas Ete Sech Golden Toy Puppet Ohio Rubén Tuesta YouTubeproinsano Globodetexto51 Decadencia777 Digital, Digital Fortnite, Digital, Digital Flow Digital, Digital Fortnite, Digital, Digital Flow Digital, Digital Fortnite, Digital, Digital Flow Digital, Digital Fortnite, Digital, Digital Flow Skibidi, Skibidi, Skibidi, Skibidi Skibidi, Skibidi, Skibidi, Skibidi Digital, Digital Fortnite, Digital, Digital Flow Digital, Digital Fortnite, Digital, Digital Flow"
+
+document.getElementById(':p').innerHTML=fl;
+
+//consola
+
+//console.log (fl.length);
+
+console.log (fl.at(10));
